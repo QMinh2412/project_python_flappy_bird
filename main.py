5e1b6a34514f68e548791a3c242b5e1ad410f12a
anhkhang121105 <trananhkhang121105@gmail.com>
2025-03-19 16:18:15 +0700

    chỉnh sửa hàm main.py (thêm vài lệnh cơ bản để show hình ảnh chim) và bird.py (vài hàm cơ bản)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-# pylint: disable=no-member
-import pygame
-
-# Khởi tạo pygame
+import pygame, sys
+from bird import Bird
 pygame.init()
 screen = pygame.display.set_mode((432,768))
 clock = pygame.time.Clock()
+bg = pygame.image.load('assets/sprites/background-day.png')
+bg = pygame.transform.scale2x(bg)
+
+bird_img = pygame.image.load('assets/sprites/redbird-midflap.png').convert()
+bird_img = pygame.transform.scale2x(bird_img)
+
+bird = Bird(100, 384, bird_img)
+
 running = True
 
 while running:
@@ -12,7 +18,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or pygame.K_UP:
+                bird.jump()
+
+
+    bird.update()
+    screen.blit(bg,(0,0))
+    bird.draw(screen)
+    pygame.display.update()
     # Tốc độ khung hình
     clock.tick(60)
 
