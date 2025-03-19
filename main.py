3562d6c4038bbfaab98213920254260a872e8236
Huy2205 <huythieu2005@gmail.com>
2025-03-19 16:37:22 +0700

    thêm background và sàn và thêm import sys vào main

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # pylint: disable=no-member
-import pygame
+import pygame, sys
+
+from ui import UI
 
 # Khởi tạo pygame
 pygame.init()
-screen = pygame.display.set_mode((432,768))
+screen = pygame.display.set_mode((432,720))
 clock = pygame.time.Clock()
-running = True
+ui = UI(screen)
 
+running = True
 while running:
-    # Xử lý sự kiện quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            sys.exit()
 
-    # Tốc độ khung hình
+    ui.background()
+    ui.floor_loop()
+    ui.draw_floor()
+    
+    pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
