216ff37f3c1c626150ad6b19f3c61e93153e4a7c
Dinh Phuoc Hung <phuochung1311@gmail.com>
2025-03-22 13:27:28 +0700

    update them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 import pygame
+from pipe import Pipe
 
 # Khởi tạo pygame
 pygame.init()
@@ -7,13 +8,22 @@ screen = pygame.display.set_mode((432,768))
 clock = pygame.time.Clock()
 running = True
 
+pipe_manager = Pipe()
+
 while running:
+    screen.fill((0,0,0))
     # Xử lý sự kiện quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pipe_manager.spawn_pipe_green:
+            pipe_manager.pipe_green_list.append(pipe_manager.create_pipe())
+
+    pipe_manager.move_pipe()
+    pipe_manager.draw_pipe(screen)
 
     # Tốc độ khung hình
+    pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
--- pipe.py
+++ pipe.py
@@ -1,51 +1,21 @@
 import pygame
-import random
 
-pygame.init()
-screen = pygame.display.set_mode((432,768))
-clock = pygame.time.Clock()
-running = True
-pipe_green = pygame.image.load('assets/sprites/pipe-green.png').convert()
-pipe_green = pygame.transform.scale2x()
-pipe_green_pos = 0
-spawn_pipe_green = pygame.USEREVENT
-pygame.time.set_timer(spawn_pipe_green, 1200)
+class Pipe:
+    def __init__(self):
+        self.pipe_green = pygame.image.load('assets/sprites/pipe-green.png').convert()
+        self.pipe_green = pygame.transform.scale2x(self.pipe_green)
+        self.pipe_green_list = []
+        self.spawn_pipe_green = pygame.USEREVENT
+        pygame.time.set_timer(self.spawn_pipe_green, 1200)
 
-pipe_green_list = []
+    def create_pipe(self):
+        new_pipe = self.pipe_green.get_rect(midtop=(500, 384))
+        return new_pipe
 
-def create_pipe():
-    new_pipe = pipe_green.get_rect(midtop=(500,384))
-    return new_pipe
+    def move_pipe(self):
+        for pipe in self.pipe_green_list:
+            pipe.centerx -= 5
 
-def move_pipe(pipes):
-    for pipe in pipes:
-        pipe.centerx -= 5
-    return pipes
-
-def draw_pipe(pipes):
-    for pipe in pipes:
-        screen.blit(pipe_green, pipe)
-
-
-while running:
-    screen.fill((0, 0, 0))
-    # Xử lý sự kiện quit
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == spawn_pipe_green:
-            pipe_green_list.append(create_pipe())
-        
-    pipe_green_list = move_pipe(pipe_green_list)
-    draw_pipe(pipe_green_list) 
-    
-    
-    
-    
-    
-    pygame.display.update()
-    # Tốc độ khung hình
-    clock.tick(60)
-
-pygame.quit()
-
+    def draw_pipe(self, screen):
+        for pipe in self.pipe_green_list:
+            screen.blit(self.pipe_green, pipe)
