98dca64afc65f79b0e25047b08f60736851eb449
Dinh Phuoc Hung <phuochung1311@gmail.com>
2025-03-23 16:32:56 +0700

    update them pipe

--- bird.py
+++ bird.py
@@ -2,13 +2,18 @@ import pygame
 
 class Bird:
     def __init__ (self):
-        bird_img = pygame.image.load('assets/sprites/redbird-midflap.png').convert()
-        bird_img = pygame.transform.scale2x(bird_img)
-        self.image = bird_img
-        self.rect = self.image.get_rect(center=(100,384))
+        self.bird_index = 0
+        upflap = pygame.transform.scale2x(pygame.image.load('assets/sprites/redbird-upflap.png').convert_alpha())
+        midflap = pygame.transform.scale2x(pygame.image.load('assets/sprites/redbird-midflap.png').convert_alpha())
+        downflap = pygame.transform.scale2x(pygame.image.load('assets/sprites/redbird-downflap.png').convert_alpha())
+        self.images = [upflap, midflap, downflap]
+        self.current_image = self.images[self.bird_index]
+        self.rect = self.current_image.get_rect(center=(100,384))
         self.gravity = 0.25
         self.movement = 0
         self.jump_strength = -8
+        self.BIRDFLAP_EVENT = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.BIRDFLAP_EVENT, 200)
 
     def update(self):
         self.movement = self.movement + self.gravity
@@ -18,8 +23,18 @@ class Bird:
         self.movement = self.jump_strength
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.rotated_image, self.rect)
+
+    def rotate(self):
+        self.rotated_image = pygame.transform.rotozoom(self.current_image, -self.movement * 3, 1)
+        self.rect = self.rotated_image.get_rect(center=self.rect.center)
+
+    def animation(self):
+        self.current_image = self.images[self.bird_index]
+        self.rotate()
     
     def restart(self):
         self.rect.center = (100, 384)
         self.movement = 0
+        self.bird_index = 0
+        self.animation()
--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 import pygame, sys
 from bird import Bird
 from ui import UI
+from pipe import Pipe
 
 
 # Hàm kiểm tra va chạm với ống và sàn
 def check_collision():
     # for pipe in pipes:
-    #     if bird_img.colliderect(pipe):
-    #         print('va cham')
+    #     if bird.rect.colliderect(pipe):
+    #          return False
     if bird.rect.top <= -75 or bird.rect.bottom >= 550:
         return False
     return True
@@ -18,18 +19,59 @@ screen = pygame.display.set_mode((432,720))
 clock = pygame.time.Clock()
 running = True
 
+
+pipe = Pipe()
 ui = UI(screen)
 bird = Bird()
 
+game_active = True
+
 while running:
-    screen.fill((0,0,0))
     # Xử lý sự kiện quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            sys.exit()
 
-    # Tốc độ khung hình
+        if event.type == pygame.KEYDOWN:
+            if event.key == (pygame.K_SPACE or pygame.K_UP) and game_active:
+                bird.jump()
+            if event.key == (pygame.K_SPACE or pygame.K_UP) and game_active == False:
+                game_active = True
+                bird.restart()
+
+        if event.type == pipe.spawn_pipe:
+            pipe.pipe_list.extend(pipe.create_pipe())   
+
+        if event.type == bird.BIRDFLAP_EVENT:
+            if bird.bird_index < 2:
+                bird.bird_index += 1
+            else:
+                bird.bird_index = 0
+            bird.animation()
+       
+    
+
+    ui.background()
+
+    # Kiểm tra game đang chạy không, nếu va chạm sẽ ngưng
+    if game_active:
+        bird.update()
+        bird.rotate()
+        bird.draw(screen)
+        pipe.move_pipe()
+        pipe.draw_pipe(screen)
+        game_active = check_collision()
+
+    # UI
+    ui.floor_loop()
+    ui.draw_floor()
+    #ui.update_score(bird, pipe)
+    ui.score_display()
+    ui.update_score(bird, pipe.pipe_list)
+
     pygame.display.update()
+
     clock.tick(60)
 
 pygame.quit()
